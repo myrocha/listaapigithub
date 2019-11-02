41a3aea64862a689fb4eb34704816675c9bd4416
mychelle <mychelle.silva.roch@gmail.com>
2019-11-02 14:19:53 -0300

    projeto que lista repositorios do git hub

--- app/src/main/java/br/com/listrepositori/github/ListRepositoryGitHubApi.kt
+++ app/src/main/java/br/com/listrepositori/github/ListRepositoryGitHubApi.kt
@@ -6,17 +6,7 @@ import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class ListRepositoryGitHubApi : DaggerApplication() {
-    /*
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-     */
-
-
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.factory().create(this)
-
-
 }
